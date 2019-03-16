c9b8d9ac9f9e0f86d8c000ce2ffe9d88cf56f96c
Tamta99 <44120812+Tamta99@users.noreply.github.com>
2019-03-16 21:43:33 +0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+include 'template.php';
+
+$template = new Template;
+
+$template->assign('Forall', 'Hello');
+$template->assign('Username', 'Tamta');
+$template->assign('sign', '!');
+
+$template->render('mytemplate');
+    
+    
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
